0fb660ed279c8f611d6cc6903cab4e2a03a3b10c
Martin Rebane <martin@linuxator.com>
2017-02-15 10:46:19 +0200

    CORS support to allow OPTION request from browser
    Needed to support Ajax requests

--- SimpleRestApp/src/main/java/ttu/tteh/SimpleRestAppApplication.java
+++ SimpleRestApp/src/main/java/ttu/tteh/SimpleRestAppApplication.java
@@ -2,6 +2,10 @@ package ttu.tteh;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 public class SimpleRestAppApplication {
@@ -9,4 +13,20 @@ public class SimpleRestAppApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SimpleRestAppApplication.class, args);
 	}
+	
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("GET");
+        config.addAllowedMethod("POST");
+        config.addAllowedMethod("PUT");
+        config.addAllowedMethod("DELETE");
+        config.addAllowedMethod("OPTIONS");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
 }
