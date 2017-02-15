17b92bada9e979edcd2320b73faac851d44c884b
Martin Rebane <martin@linuxator.com>
2017-02-15 09:43:37 +0200

    Add Spring Boot skeleton
    Unmodified download from start.spring.io

---
+++ SimpleRestApp/src/main/java/ttu/tteh/SimpleRestAppApplication.java
@@ -0,0 +1,12 @@
+package ttu.tteh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleRestAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleRestAppApplication.class, args);
+	}
+}
