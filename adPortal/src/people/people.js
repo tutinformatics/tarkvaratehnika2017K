4dff72c769012ecd9c03586687e6b2567d3835fb
Martin Rebane <martin@linuxator.com>
2017-02-28 13:17:29 +0200

    Send dummy user to Spring Boot app (POST request from Aurelia)

--- adPortal/src/people/people.js
+++ adPortal/src/people/people.js
@@ -1,3 +1,5 @@
+import {HttpClient, json} from 'aurelia-fetch-client'
+
 export class people{
 
 	constructor() {
@@ -5,4 +7,24 @@ export class people{
 		this.count = 0
 	}
 
+	addUser() {
+		let client = new HttpClient();
+
+		let userData = {
+			"firstName": "MyName",
+			"lastName": "MyLAstName",
+			"numOfPets": 11
+		}
+
+		client.fetch('http://localhost:8080/users/add', {
+			'method': "POST",
+			'body': json(userData)
+		})
+			.then(response => response.json())
+			.then(data => {
+				console.log("Server saatis " + data.firstName);
+		});
+
+		console.log("Method executed!")
+	}
 }
