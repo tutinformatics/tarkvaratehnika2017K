9436f1ad8392a44ae1864bd2c5ee54b8b06ce6c5
Martin Rebane <martin@linuxator.com>
2017-03-06 12:04:00 +0200

    Display existing users from REST in Aurelia app

--- adPortal/src/people/people.js
+++ adPortal/src/people/people.js
@@ -3,10 +3,19 @@ import {HttpClient, json} from 'aurelia-fetch-client'
 export class people{
 
 	userData = {}
+	userList = []
 
 	constructor() {
 		this.appName = "adPortal"
 		this.count = 0
+	}
+
+	activate() {
+		let client = new HttpClient();
+
+		client.fetch('http://localhost:8080/users')
+			.then(response => response.json())
+			.then(users => this.userList = users);
 	}
 
 	addUser() {
