4c588337bb163c72ec726cefeb7f9234ff43bd88
Martin Rebane <martin@linuxator.com>
2018-04-24 22:50:30 +0100

    Add UI and JS code for authentication popup

--- adPortal/src/home/index.js
+++ adPortal/src/home/index.js
@@ -1,6 +1,21 @@
+import {AuthService} from 'aurelia-auth';
+import {inject} from 'aurelia-framework';
+
+@inject(AuthService)
 export class Home {
 
-	constructor() {
-		this.message = "Just checking..."
-	}
+    constructor(auth){
+        this.auth = auth;
+        this.message = "Just checking..."
+    };
+
+    authenticate(provider){
+    	let url = "http://localhost:9000/login";
+    	console.log(url)
+    	console.log(provider)
+        return this.auth.authenticate(provider, url)
+        .then((response)=>{
+            console.log("auth response " + response);
+        });
+    }
 }
