7da7d56c74f62db3b8582087cf5c6ec99dc4faa9
Martin Rebane <martin@linuxator.com>
2018-04-24 22:50:02 +0100

    Modify authConfig.js to fit our needs

--- adPortal/src/authConfig.js
+++ adPortal/src/authConfig.js
@@ -1,14 +1,8 @@
 var configForDevelopment = {
+    
     providers: {
         google: {
-            clientId: '239531826023-ibk10mb9p7ull54j55a61og5lvnjrff6.apps.googleusercontent.com'
-        }
-        ,
-        linkedin:{
-            clientId:'778mif8zyqbei7'
-        },
-        facebook:{
-            clientId:'1452782111708498'
+            clientId: '93685841879-p0qhjbe4irgsgcv2v5dujl148dujd9u4.apps.googleusercontent.com'
         }
     }
 };
@@ -16,16 +10,8 @@ var configForDevelopment = {
 var configForProduction = {
     providers: {
         google: {
-            clientId: '239531826023-3ludu3934rmcra3oqscc1gid3l9o497i.apps.googleusercontent.com'
+            clientId: '93685841879-p0qhjbe4irgsgcv2v5dujl148dujd9u4.apps.googleusercontent.com'
         }
-        ,
-        linkedin:{
-            clientId:'7561959vdub4x1'
-        },
-        facebook:{
-            clientId:'1653908914832509'
-        }
-
     }
 };
 var config ;
