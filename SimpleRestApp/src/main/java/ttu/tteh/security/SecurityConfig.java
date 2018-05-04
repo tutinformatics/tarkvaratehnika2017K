cb95b0d29f655b1048e867176751452983cf1ca7
Martin Rebane <martin@linuxator.com>
2018-05-04 22:48:32 +0100

    Add Spring Security dependency and create SecurityConfig template

---
+++ SimpleRestApp/src/main/java/ttu/tteh/security/SecurityConfig.java
@@ -0,0 +1,12 @@
+package ttu.tteh.security;
+
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+}
