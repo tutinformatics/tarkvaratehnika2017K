1a4ebe2475ce697b60b2d5c9c743fa977cac364e
Martin Rebane <martin@linuxator.com>
2018-05-06 01:04:51 +0100

    Add principal to AuthenticationData (save user data)

--- SimpleRestApp/src/main/java/ttu/tteh/auth/AuthenticationData.java
+++ SimpleRestApp/src/main/java/ttu/tteh/auth/AuthenticationData.java
@@ -4,13 +4,16 @@ import java.util.Collection;
 
 import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 public class AuthenticationData extends AbstractAuthenticationToken{
 
 	private static final long serialVersionUID = 1L;
+	private UserDetails principal;
 
-	public AuthenticationData(Collection<? extends GrantedAuthority> authorities) {
+	public AuthenticationData(Collection<? extends GrantedAuthority> authorities, UserDetails principal) {
 		super(authorities);
+		this.principal = principal;
 	}
 
 	@Override
@@ -20,6 +23,6 @@ public class AuthenticationData extends AbstractAuthenticationToken{
 
 	@Override
 	public Object getPrincipal() {
-		return null;
+		return principal;
 	}
 }
